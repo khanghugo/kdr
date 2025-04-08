cc3316a61055c27f38a74e38411292ac54158d8e
khang <mkhangle20@gmail.com>
2025-04-08 09:05:56 -0400

    api server now runs in release for more accurate performance

--- run_api_server.sh
+++ run_api_server.sh
@@ -1,2 +1,2 @@
 #/bin/usr/env sh
-cargo run --package rest-api-server --target x86_64-unknown-linux-gnu --features cors -- $@
+cargo run --package rest-api-server --target x86_64-unknown-linux-gnu --features cors --release -- $@
