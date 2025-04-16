61042fb6e41284b7f429bba886108f9ef200b251
khang <mkhangle20@gmail.com>
2025-04-15 20:32:44 -0400

    make it not crash on maps with no lightmap

--- kdr/src/renderer/bsp_lightmap.rs
+++ kdr/src/renderer/bsp_lightmap.rs
@@ -101,118 +101,109 @@ impl LightMapAtlasBuffer {
 
         let mut atlas_image = RgbaImage::new(DIMENSION, DIMENSION);
 
-        bsp.faces.iter().enumerate().for_each(|(idx, face)| {
-            // let tex_info = &bsp.texinfo[face.texinfo as usize];
-            // let texture = &bsp.textures[tex_info.texture_index as usize];
-            if face.lightmap_offset == -1 {
-                return;
-            }
+        let has_light_map = bsp.lightmap.len() > 1;
 
-            let face_vertices = face_vertices(face, bsp);
-            let texinfo = &bsp.texinfo[face.texinfo as usize];
-
-            let vertices_texcoords: Vec<[f32; 2]> = face_vertices
-                .iter()
-                .map(|pos| vertex_uv(pos, &texinfo))
-                .collect();
-
-            let lightmap_dimensions = get_lightmap_dimensions(&vertices_texcoords);
-
-            let alloc_width = lightmap_dimensions.width + 2 * PADDING;
-            let alloc_height = lightmap_dimensions.height + 2 * PADDING;
-
-            let allocation = atlas
-                .allocate(guillotiere::size2(alloc_width, alloc_height))
-                .expect("cannot allocate lightmap atlas");
-
-            // very easy to get things wrong, dont touch too much
-            let atlas_allocation = LightMapAtlasAllocation {
-                atlas_x: (allocation.rectangle.min.x + PADDING) as f32 / DIMENSION as f32,
-                atlas_y: (allocation.rectangle.min.y + PADDING) as f32 / DIMENSION as f32,
-                atlas_width: (lightmap_dimensions.width) as f32 / DIMENSION as f32,
-                atlas_height: (lightmap_dimensions.height) as f32 / DIMENSION as f32,
-                // min_uv belongs to texture coordinate of the current face, not the lightmap
-                min_x: lightmap_dimensions.min_u as f32,
-                min_y: lightmap_dimensions.min_v as f32,
-                lightmap_width: lightmap_dimensions.width as f32,
-                lightmap_height: lightmap_dimensions.height as f32,
-            };
-
-            allocations.insert(idx, atlas_allocation);
-
-            let lightmap_run_end = lightmap_dimensions.height * lightmap_dimensions.width;
-
-            assert_eq!(face.lightmap_offset % 3, 0);
-            let tupled_offset = face.lightmap_offset as usize / 3; // the the original offset is on byte but we have rgb
-
-            let lightmap_run =
-                &bsp.lightmap[tupled_offset..(tupled_offset + lightmap_run_end as usize)];
-
-            // main texture
-            for y in 0..(lightmap_dimensions.height) {
-                for x in 0..(lightmap_dimensions.width) {
-                    let curr_element = x + y * (lightmap_dimensions.width);
-                    let curr_pixel = lightmap_run[curr_element as usize];
-                    let curr_rgba = [curr_pixel[0], curr_pixel[1], curr_pixel[2], 255];
-
-                    atlas_image.put_pixel(
-                        (x + allocation.rectangle.min.x + PADDING) as u32,
-                        (y + allocation.rectangle.min.y + PADDING) as u32,
-                        image::Rgba(curr_rgba),
-                    );
+        if has_light_map {
+            bsp.faces.iter().enumerate().for_each(|(idx, face)| {
+                // let tex_info = &bsp.texinfo[face.texinfo as usize];
+                // let texture = &bsp.textures[tex_info.texture_index as usize];
+                if face.lightmap_offset == -1 {
+                    return;
                 }
-            }
 
-            {
-                let original_width = lightmap_dimensions.width;
-                let original_height = lightmap_dimensions.height;
+                let face_vertices = face_vertices(face, bsp);
+                let texinfo = &bsp.texinfo[face.texinfo as usize];
 
-                for y in 0..alloc_height {
-                    for x in 0..alloc_width {
-                        // Only process padding areas
-                        if x >= PADDING
-                            && x < alloc_width - PADDING
-                            && y >= PADDING
-                            && y < alloc_height - PADDING
-                        {
-                            continue;
-                        }
+                let vertices_texcoords: Vec<[f32; 2]> = face_vertices
+                    .iter()
+                    .map(|pos| vertex_uv(pos, &texinfo))
+                    .collect();
 
-                        // Calculate source coordinates with mirroring
-                        let src_x = (x - PADDING).clamp(0, original_width - 1).max(0);
-                        let src_y = (y - PADDING).clamp(0, original_height - 1).max(0);
+                let lightmap_dimensions = get_lightmap_dimensions(&vertices_texcoords);
 
-                        if let Some(pixel) = lightmap_run
-                            .get(src_x as usize + src_y as usize * original_width as usize)
-                        {
-                            let dest_x = allocation.rectangle.min.x + x;
-                            let dest_y = allocation.rectangle.min.y + y;
-                            atlas_image.put_pixel(
-                                dest_x as u32,
-                                dest_y as u32,
-                                image::Rgba([pixel[0], pixel[1], pixel[2], 255]),
-                            );
+                let alloc_width = lightmap_dimensions.width + 2 * PADDING;
+                let alloc_height = lightmap_dimensions.height + 2 * PADDING;
+
+                let allocation = atlas
+                    .allocate(guillotiere::size2(alloc_width, alloc_height))
+                    .expect("cannot allocate lightmap atlas");
+
+                // very easy to get things wrong, dont touch too much
+                let atlas_allocation = LightMapAtlasAllocation {
+                    atlas_x: (allocation.rectangle.min.x + PADDING) as f32 / DIMENSION as f32,
+                    atlas_y: (allocation.rectangle.min.y + PADDING) as f32 / DIMENSION as f32,
+                    atlas_width: (lightmap_dimensions.width) as f32 / DIMENSION as f32,
+                    atlas_height: (lightmap_dimensions.height) as f32 / DIMENSION as f32,
+                    // min_uv belongs to texture coordinate of the current face, not the lightmap
+                    min_x: lightmap_dimensions.min_u as f32,
+                    min_y: lightmap_dimensions.min_v as f32,
+                    lightmap_width: lightmap_dimensions.width as f32,
+                    lightmap_height: lightmap_dimensions.height as f32,
+                };
+
+                allocations.insert(idx, atlas_allocation);
+
+                let lightmap_run_end = lightmap_dimensions.height * lightmap_dimensions.width;
+
+                assert_eq!(face.lightmap_offset % 3, 0);
+                let tupled_offset = face.lightmap_offset as usize / 3; // the the original offset is on byte but we have rgb
+
+                let lightmap_run =
+                    &bsp.lightmap[tupled_offset..(tupled_offset + lightmap_run_end as usize)];
+
+                // main texture
+                for y in 0..(lightmap_dimensions.height) {
+                    for x in 0..(lightmap_dimensions.width) {
+                        let curr_element = x + y * (lightmap_dimensions.width);
+                        let curr_pixel = lightmap_run[curr_element as usize];
+                        let curr_rgba = [curr_pixel[0], curr_pixel[1], curr_pixel[2], 255];
+
+                        atlas_image.put_pixel(
+                            (x + allocation.rectangle.min.x + PADDING) as u32,
+                            (y + allocation.rectangle.min.y + PADDING) as u32,
+                            image::Rgba(curr_rgba),
+                        );
+                    }
+                }
+
+                {
+                    let original_width = lightmap_dimensions.width;
+                    let original_height = lightmap_dimensions.height;
+
+                    for y in 0..alloc_height {
+                        for x in 0..alloc_width {
+                            // Only process padding areas
+                            if x >= PADDING
+                                && x < alloc_width - PADDING
+                                && y >= PADDING
+                                && y < alloc_height - PADDING
+                            {
+                                continue;
+                            }
+
+                            // Calculate source coordinates with mirroring
+                            let src_x = (x - PADDING).clamp(0, original_width - 1).max(0);
+                            let src_y = (y - PADDING).clamp(0, original_height - 1).max(0);
+
+                            if let Some(pixel) = lightmap_run
+                                .get(src_x as usize + src_y as usize * original_width as usize)
+                            {
+                                let dest_x = allocation.rectangle.min.x + x;
+                                let dest_y = allocation.rectangle.min.y + y;
+                                atlas_image.put_pixel(
+                                    dest_x as u32,
+                                    dest_y as u32,
+                                    image::Rgba([pixel[0], pixel[1], pixel[2], 255]),
+                                );
+                            }
                         }
                     }
                 }
-            }
-
-            // border
-            // {
-            //     let border_color = [255, 0, 255, 255]; // Purple for visibility
-            //     for x in allocation.rectangle.min.x..allocation.rectangle.max.x {
-            //         for y in allocation.rectangle.min.y..allocation.rectangle.max.y {
-            //             if x == allocation.rectangle.min.x
-            //                 || x == allocation.rectangle.max.x - 1
-            //                 || y == allocation.rectangle.min.y
-            //                 || y == allocation.rectangle.max.y - 1
-            //             {
-            //                 atlas_image.put_pixel(x as u32, y as u32, image::Rgba(border_color));
-            //             }
-            //         }
-            //     }
-            // }
-        });
+            });
+        } else {
+            // just make it all white, otherwise, it will look bad
+            atlas_image.fill(255u8);
+        }
 
         let (width, height) = atlas_image.dimensions();
 
